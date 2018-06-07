62469ff3405fc52490f2a3006a7b582fee306c16
Joye.Chen(陳名榆) <joye.chen@vivotek.com>
2018-06-07 16:03:31 +0800

    [M] Intermediate product, Subject can notify with object instead of parameter, while Observer  still has to hadle different update()

--- observer_weather_station.cpp
+++ observer_weather_station.cpp
@@ -12,25 +12,37 @@ public:
 class Subject {
 
 public:
+	
+	class Item
+	{
+	public:
+		virtual void showItem() = 0;
+	};
+	
 	virtual void registerObserver(Observer *observer) = 0;
 	virtual void removeObserver(Observer *observer) = 0;
-	virtual void notifyObservers() = 0;
+	//virtual void notifyObservers(Item *item) = 0;
 
 };
-
 
 class WeatherData : public Subject {
 	std::vector<Observer *> observers; 
 
-	float temp = 0.0;
-	float humidity = 0.0;
-	float pressure = 0.0;
-
 public:
+	
+	class SubjectItem : public Subject::Item
+	{
+	public:
+		float temp = 0.0;
+		float humidity = 0.0;
+		float pressure = 0.0;
+		void showItem() override;
+	};
+	
 	void registerObserver(Observer *observer) override;
 	void removeObserver(Observer *observer) override;
-	void notifyObservers() override;
-	void setState(float temp, float humidity, float pressure);
+	void notifyObservers(SubjectItem *subjectitem);
+	void setState(SubjectItem *subjectitem);
 
 };
 
@@ -43,6 +55,12 @@ public:
 	virtual void update(float temp, float humidity, float pressure) override;
 
 };
+
+
+void WeatherData::SubjectItem::showItem(){
+	std::cout << "WeatherData Subject Item" << std::endl;
+}
+
 
 void WeatherData::registerObserver(Observer *observer) {
 	observers.push_back(observer);
@@ -57,18 +75,19 @@ void WeatherData::removeObserver(Observer *observer) {
 	}
 }
 
-void WeatherData::notifyObservers() {
+void WeatherData::notifyObservers(WeatherData::SubjectItem *ws) {
 	for (Observer *observer : observers) { // notify all observers
-		observer->update(temp, humidity, pressure);
+		observer->update(ws->temp, ws->humidity, ws->pressure);
 	}
 }
 
-void WeatherData::setState(float temp, float humidity, float pressure) {
-	this->temp = temp;
-	this->humidity = humidity;
-	this->pressure = pressure;
+void WeatherData::setState(WeatherData::SubjectItem *ws) {
+	WeatherData::SubjectItem ws_o;
+	ws_o.temp = ws->temp;
+	ws_o.humidity = ws->humidity;
+	ws_o.pressure = ws->pressure;
 	std::cout << std::endl;
-	notifyObservers();
+	notifyObservers(&ws_o);
 }
 
 void Display::update(float temp, float humidity, float pressure) {
@@ -86,25 +105,24 @@ Display::Display(int id) {
 
 int main() {
 	WeatherData weatherStation;
+	WeatherData::SubjectItem ws;
 	Display one(1), two(2), three(3);
 
-	float temp, humidity, pressure;
-
+	ws.showItem();
 	weatherStation.registerObserver(&one);
 	weatherStation.registerObserver(&two);
 	weatherStation.registerObserver(&three);
 
 	std::cout << "Enter Temperature, Humidity, Pressure (seperated by spaces) << ";
-	std::cin >> temp >> humidity >> pressure;
-
-	weatherStation.setState(temp, humidity, pressure);
+	std::cin >> ws.temp >> ws.humidity >> ws.pressure;
+	weatherStation.setState(&ws);
 
 	weatherStation.removeObserver(&two);
 
 	std::cout << "\n\nEnter Temperature, Humidity, Pressure (seperated by spaces) << ";
-	std::cin >> temp >> humidity >> pressure;
+	std::cin >> ws.temp >> ws.humidity >> ws.pressure;
 
-	weatherStation.setState(temp, humidity, pressure);
+	weatherStation.setState(&ws);
 
 	return 0;
 }
