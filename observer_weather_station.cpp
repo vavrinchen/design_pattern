12df8372f90a5626f6787148bca4ce00d7a432d7
joye.chen <joye.chen@vivotek.com>
2018-06-13 21:18:27 +0800

    [M] 1.Observer can update by different Subject

--- observer_weather_station.cpp
+++ observer_weather_station.cpp
@@ -1,128 +1,185 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include "Subject.h"
+#include "Observer.h"
 
-class Observer {
 
-public:
-	virtual void update(float temp, float humidity, float pressure) = 0;
 
-};
 
-class Subject {
+class WeatherData : public Subject
+{
+	std::vector<Observer *> observers;
 
-public:
-	
-	class Item
-	{
-	public:
-		virtual void showItem() = 0;
-	};
-	
-	virtual void registerObserver(Observer *observer) = 0;
-	virtual void removeObserver(Observer *observer) = 0;
-	//virtual void notifyObservers(Item *item) = 0;
+	float temp = 0.0;
+	float humidity = 0.0;
+	float pressure = 0.0;
 
-};
-
-class WeatherData : public Subject {
-	std::vector<Observer *> observers; 
-
-public:
-	
-	class SubjectItem : public Subject::Item
-	{
-	public:
-		float temp = 0.0;
-		float humidity = 0.0;
-		float pressure = 0.0;
-		void showItem() override;
-	};
-	
+  public:
 	void registerObserver(Observer *observer) override;
 	void removeObserver(Observer *observer) override;
-	void notifyObservers(SubjectItem *subjectitem);
-	void setState(SubjectItem *subjectitem);
-
+	void notifyObservers() override;
+	void setState(float temp, float humidity, float pressure);
+	void getState() override;
 };
 
-class Display : public Observer {
+class Display : public Observer
+{
 
 	int id;
 
-public:
+  public:
 	Display(int id);
-	virtual void update(float temp, float humidity, float pressure) override;
-
+	virtual void update(Subject *s) override;
 };
 
-
-void WeatherData::SubjectItem::showItem(){
-	std::cout << "WeatherData Subject Item" << std::endl;
-}
-
-
-void WeatherData::registerObserver(Observer *observer) {
+void WeatherData::registerObserver(Observer *observer)
+{
 	observers.push_back(observer);
 }
 
-void WeatherData::removeObserver(Observer *observer) {
+void WeatherData::removeObserver(Observer *observer)
+{
 	// find the observer
 	auto iterator = std::find(observers.begin(), observers.end(), observer);
 
-	if (iterator != observers.end()) { 
-		observers.erase(iterator); 
+	if (iterator != observers.end())
+	{
+		observers.erase(iterator);
 	}
 }
 
-void WeatherData::notifyObservers(WeatherData::SubjectItem *ws) {
-	for (Observer *observer : observers) { // notify all observers
-		observer->update(ws->temp, ws->humidity, ws->pressure);
+void WeatherData::notifyObservers()
+{
+	for (Observer *observer : observers)
+	{ // notify all observers
+		observer->update(this);
 	}
 }
 
-void WeatherData::setState(WeatherData::SubjectItem *ws) {
-	WeatherData::SubjectItem ws_o;
-	ws_o.temp = ws->temp;
-	ws_o.humidity = ws->humidity;
-	ws_o.pressure = ws->pressure;
+void WeatherData::setState(float temp, float humidity, float pressure)
+{
+	this->temp = temp;
+	this->humidity = humidity;
+	this->pressure = pressure;
 	std::cout << std::endl;
-	notifyObservers(&ws_o);
+	notifyObservers();
 }
 
-void Display::update(float temp, float humidity, float pressure) {
+void WeatherData::getState()
+{
+	std::cout << "---Get Data---\tTemperature: " << this->temp
+			  << "\tHumidity: " << this->humidity
+			  << "\tPressure: " << this->pressure
+			  << std::endl;
+}
 
+class HealthData : public Subject
+{
+	std::vector<Observer *> observers;
+
+	float height = 0.0;
+	float weight = 0.0;
+
+  public:
+	void registerObserver(Observer *observer) override;
+	void removeObserver(Observer *observer) override;
+	void notifyObservers() override;
+	void setState(float height, float weight);
+	void getState() override;
+};
+
+void HealthData::registerObserver(Observer *observer)
+{
+	observers.push_back(observer);
+}
+
+void HealthData::removeObserver(Observer *observer)
+{
+	// find the observer
+	auto iterator = std::find(observers.begin(), observers.end(), observer);
+
+	if (iterator != observers.end())
+	{
+		observers.erase(iterator);
+	}
+}
+
+void HealthData::notifyObservers()
+{
+	for (Observer *observer : observers)
+	{ // notify all observers
+		observer->update(this);
+	}
+}
+
+void HealthData::setState(float height, float weight)
+{
+	this->height = height;
+	this->weight = weight;
+	std::cout << std::endl;
+	notifyObservers();
+}
+
+void HealthData::getState()
+{
+	std::cout << "---Get Data---\tHeight: " << this->height
+			  << "\tWeight: " << this->weight
+			  << std::endl;
+}
+
+void Display::update(Subject *s)
+{
+	s->getState();
+	/*
 	std::cout << "---Display (" << id << ") Data---\tTemperature: " << temp
-		<< "\tHumidity: " << humidity
-		<< "\tPressure: " << pressure
-		<< std::endl;
+			  << "\tHumidity: " << humidity
+			  << "\tPressure: " << pressure
+			  << std::endl;
+			  */
+
 }
 
-Display::Display(int id) {
+Display::Display(int id)
+{
 	this->id = id;
 }
 
-
-int main() {
+int main()
+{
 	WeatherData weatherStation;
-	WeatherData::SubjectItem ws;
+	HealthData healthStation;
 	Display one(1), two(2), three(3);
 
-	ws.showItem();
+	float temp, humidity, pressure, height, weight;
+
 	weatherStation.registerObserver(&one);
+	healthStation.registerObserver(&one);
 	weatherStation.registerObserver(&two);
+	healthStation.registerObserver(&two);
 	weatherStation.registerObserver(&three);
 
 	std::cout << "Enter Temperature, Humidity, Pressure (seperated by spaces) << ";
-	std::cin >> ws.temp >> ws.humidity >> ws.pressure;
-	weatherStation.setState(&ws);
+	std::cin >> temp >> humidity >> pressure;
+
+	weatherStation.setState(temp, humidity, pressure);
+
+	std::cout << "Enter Height, Weight (seperated by spaces) << ";
+	std::cin >> height >> weight;
+
+	healthStation.setState(height, weight);
 
 	weatherStation.removeObserver(&two);
+	healthStation.removeObserver(&two);
 
 	std::cout << "\n\nEnter Temperature, Humidity, Pressure (seperated by spaces) << ";
-	std::cin >> ws.temp >> ws.humidity >> ws.pressure;
+	std::cin >> temp >> humidity >> pressure;
 
-	weatherStation.setState(&ws);
+	std::cout << "\n\nEnter Height, Weight (seperated by spaces) << ";
+	std::cin >> height >> weight;
+
+	weatherStation.setState(temp, humidity, pressure);
+	healthStation.setState(height, weight);
 
 	return 0;
 }
